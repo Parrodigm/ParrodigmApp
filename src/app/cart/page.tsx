458af12025039bc66c9363c4e1c03c5b44524868
사민주 <saminju@saminjuui-MacBookPro.local>
2025-04-26 00:09:36 -0700

    feat: cart layout

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,16 +1,40 @@
 "use client";
 import { ProductDetailCard } from "@/src/components/product/ProductDetailCard";
-import { Flex } from "@/styled-system/jsx";
+import { Box, Flex } from "@/styled-system/jsx";
 import { useLocalStorage } from "usehooks-ts";
 import { CartItem } from "../products/[id]/layout";
 import { products } from "@/src/products";
-
+import { Text } from "@/src/components/Text";
+import Trash from "@/public/Trash.svg";
 export default function Basket() {
   const [cartList, setCartList] = useLocalStorage<CartItem[]>("cartList", []);
   console.log(cartList);
   return (
-    <Flex direction="column" gap="28px" align="center">
-      {cartList.map((el) => {
+    <Flex
+      direction="column"
+      align="center"
+      height="100%"
+      marginTop="10px"
+      borderTop="3px solid #6294FF"
+      padding="0 20px"
+      borderTopRadius="40px"
+      position="relative"
+    >
+      <Box
+        position="absolute"
+        top="-4px"
+        width="120px"
+        height="6px"
+        bg="#6294FF"
+        borderRadius="full"
+      />
+      <Flex justifyContent="space-between" width="100%" marginTop="20px">
+        <Text color="#6294FF" fontSize="24px" fontWeight="semibold">
+          Shopping Cart
+        </Text>
+        <Trash />
+      </Flex>
+      {/* {cartList.map((el) => {
         const itemId = el.item.id;
         return (
           <ProductDetailCard
@@ -22,7 +46,7 @@ export default function Basket() {
             price={products[itemId - 1].price}
           />
         );
-      })}
+      })} */}
     </Flex>
   );
 }
